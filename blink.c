db589ea9ac429c388437840c7492fb4d5701ee51
Evan Allen <evanallen13@github.com>
2025-12-14 20:37:09 -0500

    Correct print formatting and add semicolon in blink.c
    Fix print statement to format output correctly and add missing semicolon.

--- blink.c
+++ blink.c
@@ -41,7 +41,9 @@ int main() {
     // All ON
     for (i = 0; i < NUM_PINS; i++) {
         values[i] = 0;
-        printf("Turning on: %d\n",  gpio_pins[i]);
+        printf("Turning on: ",  gpio_pins[i]);
+        sleep(1)
+        printf("%d\n",  gpio_pins[i]);
     }
     gpiod_line_request_set_values(request, values);
     sleep(1);
